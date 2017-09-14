acce75abe5f32010f2b4931196fd08603f90fb3c
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-09-14 14:40:16 +0100

    use google-spreadsheet

--- lambda/src/config.js
+++ lambda/src/config.js
@@ -1,7 +1,5 @@
 import AWS from 'aws-sdk'
 
-let fs = require('fs');
-let google = require('googleapis');
 let s3 = new AWS.S3();
 
 export function loadFromS3() {
@@ -9,37 +7,15 @@ export function loadFromS3() {
    console.log("running in stage " + stage)
    return new Promise((resolve, reject) =>{
      s3.getObject({ Bucket: "guconf-flexible/editorial-tools-help", Key: "test.json" }, (err, creds) => {
-         if(err){
-             console.error("failed to retreive credentials", err)
-             reject(err)
-             return
-         }
-         let string = creds.Body.toString();
-         let j = JSON.parse(string);
-         console.log(j);
-         resolve(j);
-    })
-})
-
-}
-
-export function googleCreds() {
-  let fileName = 'g-keys.json'
-  new Promise((resolve, reject) =>{
-  let data = s3.getObject({ Bucket: "guconf-flexible/editorial-tools-help", Key: fileName })
-  let credsFile = fs.createWriteStream("/tmp/" + fileName);
-  data.createReadStream().pipe(credsFile);
-  resolve("downloaded Google credentials");
-  let readCreds = fs.readFile("/tmp/" + fileName, (err, data) => {
-    console.log(err);
-    console.log(data);
-    })
-  });
-
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = "/tmp/" + fileName;
-
-  google.auth.getApplicationDefault(function(err, authClient) {
-      if (err) {
-          console.log(err);
-      }});
+       if(err){
+         console.error("failed to retreive credentials", err)
+         reject(err)
+         return
+       }
+       let string = creds.Body.toString();
+       let j = JSON.parse(string);
+       console.log(j);
+       resolve(j);
+     })
+   })
 }
--- lambda/src/index.js
+++ lambda/src/index.js
@@ -1,31 +1,19 @@
 import AWS from 'aws-sdk';
-import {loadFromS3, googleCreds} from './config'
-import repeat from 'lodash/repeat'
-import 'whatwg-fetch';
+import {loadFromS3} from './config'
+import {getSheetsJson} from './google'
+
+let GoogleSpreadsheet = require('google-spreadsheet');
 
 export function handler(event, context, callback) {
   console.log("Initialising.")
-  let creds = loadFromS3()
-  creds.then((creds)=>{
-    console.log(creds)
-    let gCreds = googleCreds();
-    console.log("CREDS = " + process.env.GOOGLE_APPLICATION_CREDENTIALS);
-    console.log(repeat('hello',10))
-    //callback(null)
+  let config = loadFromS3()
+  config.then((config)=> {
+    let sheetJson = getSheetsJson().then(function(response) {
+      console.log(response);
+    });
+    // callback(null)
   }).catch((error)=>{
     console.error("Failed to get credentials",error)
     callback(error)
-  })
-
-}
-
-function getGoogleSheetsData() {
-  let sheetId = '16yi_4-_JYd8CH-UyZ0cbWVtVUqA2DUgNoiqSUwt2FJc' //TODO: Move to config
-  fetch('https://sheets.googleapis.com/v4/spreadsheets/values:batchGet?ranges=Sheet1' , { //TODO: Determine range
-    method: 'GET'
-  }).then(function(response) {
-    if (response.status === 200) {
-
-    }
   })
 }
