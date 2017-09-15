2dd650384e75a18c15eb7c7a5b522a2a4e93be8e
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-09-15 16:01:48 +0100

    upload to s3

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -2,13 +2,23 @@ import AWS from 'aws-sdk';
 import {loadFromS3} from './config'
 import {getSheetsJson} from './google'
 
+let s3 = new AWS.S3();
+
 export function handler(event, context, callback) {
   console.log("Initialising.")
   let config = loadFromS3()
   config.then((conf)=> {
     let sheetJson = getSheetsJson(conf).then(function(response) {
       let parsed = response.map(parseRows);
-      console.log('parsed = ', parsed);
+      let uploadParams = {
+        Bucket: "editorial-tools-help",
+        Key: "data.json",
+        Body: JSON.stringify(parsed)
+      };
+      let upload = s3.putObject(uploadParams, function(err, data) {
+        if (err) console.error("could not upload to s3", err);
+        else console.log('successfully uploaded to s3')
+      })
     });
     // callback(null)
   }).catch((error)=>{
